4a776cd6983abc5ddb02fe46fc88e2870103fb62
Adam.chen <adam.chen@infotech.com.tw>
2025-03-17 19:09:11 +0800

    update bash

---
+++ session-01/words.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+for word in $(cat words.txt | tr -s '[:blank:]' | tr -d "\n" | tr -d '[:punct:]' |  tr A-Z a-z)
+do
+  printf '%s\n' "${word//[[:space:]]}" >> temp.txt
+done
+
+cat temp.txt | sort | uniq -c | sort -r
