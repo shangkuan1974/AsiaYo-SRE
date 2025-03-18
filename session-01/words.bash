e7c1bc82f2a09074965ff6eba03cc23deb88f5d1
Adam.chen <adam.chen@infotech.com.tw>
2025-03-18 09:14:34 +0800

    add note with session-01

--- session-01/words.bash
+++ session-01/words.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
-for word in $(cat words.txt | tr -s '[:blank:]' | tr -d "\n" | tr -d '[:punct:]' |  tr A-Z a-z)
+# 想法:
+# 是將多行自串先轉換成單行字串，然後再去取單字去除標點符號後，再將所得的轉換成小寫，在用 for回圈取出所有元素，讓每個元素為單獨一行，寫入 temp.txt 
+# 然後以uniq的方式列出重複製串的統計，然後遞減的方式排列統計數字，取其第一筆
+
+for word in $(cat words.txt | tr "\n" " " | tr -d '[:punct:]' |  tr A-Z a-z)
 do
-  printf '%s\n' "${word//[[:space:]]}" >> temp.txt
+  echo $word >> temp.txt
 done
 
 cat temp.txt | sort | uniq -c | sort -r | head -n 1
+rm temp.txt
